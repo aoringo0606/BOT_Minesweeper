a31d218fef3b81e066eb834cc9edafd9cc05a2a9
aoringo0606 <heizou0789@gmail.com>
2026-05-02 11:39:08 +0900

    使い方説明の後、hardが表示されるバグを修正

--- index.ts
+++ index.ts
@@ -21,6 +21,7 @@ client.on('MESSAGE_CREATED', async ({ body }) => {
     if (!plainText.includes('easy') && !plainText.includes('normal') && !plainText.includes('hard') && !plainText.includes('extreme')){
         message = "how to use\n`@BOT_Minesweeper mode (easy / normal / hard / extreme)`\neasy: 9x9, 10 mines\nnormal: 16x16, 40 mines\nhard: 30x16, 99mines\nextreme: 9x9, 67mines(14 safe cells)";
         await api.channels.postMessage(channelId, { content: message, embed: true });
+        return;
     }
     const width = plainText.includes('easy') ? 9 : plainText.includes('normal') ? 16 : plainText.includes('extreme') ? 9 : 30;
     const height = plainText.includes('easy') ? 9 : plainText.includes('normal') ? 16 : plainText.includes('extreme') ? 9 : 16;
